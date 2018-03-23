f3f5cac570de78e466b954b03da78c33a83abb98
beangoben <beangoben@gmail.com>
2018-03-23 12:49:31 -0400

    some plot utils

---
+++ src/visualization/plot_utils.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+import matplotlib.patches as mpatches
+import numpy as np
+
+
+def dist_stats_box(y):
+    result = stats.describe(y)
+    ax = plt.gca()
+    handles, labels = ax.get_legend_handles_labels()
+    ex_labels = []
+    # ex_labels.append('$n=%d$' % (result.nobs))
+    ex_labels.append('$\mu=%2.2f$' % (result.mean))
+    ex_labels.append('$med=%2.2f$' % (np.median(y)))
+    ex_labels.append('$\sigma=%2.2f$' % (np.sqrt(result.variance)))
+    ex_labels.append('$\min=%2.2f$' % (result.minmax[0]))
+    ex_labels.append('$\max=%2.2f$' % (result.minmax[1]))
+    ex_handles = [mpatches.Patch(
+        color='white', alpha=0.0, visible=False) for i in ex_labels]
+    plt.legend(handles=handles + ex_handles, labels=labels + ex_labels,
+               loc='best', frameon=False)
+
+    return
