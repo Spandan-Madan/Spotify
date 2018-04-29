502dbf058a2cf3ef778ebcf75ce108d8340008c4
beangoben <beangoben@gmail.com>
2018-04-29 18:57:04 -0400

    updated basic feature

--- src/features/base_feature.py
+++ src/features/base_feature.py
@@ -1,8 +1,32 @@
 
 class Feature(object):
 
-    def __init__(self):
+    def __init__(self, subset=''):
+        '''
+        Feature constructor, will load all relevant data, if using the 5k subset, use the string "5k-"
+        Input: can be whatever you need (dimention of embedding, folder,etc)
+        '''
 
-    def load(self):
+        return
 
-    def
+    def transform(self,turis)
+        '''
+        Convert track a list of turis to a vector representation.
+        Input: turis is a list of strings of length n
+        Output: will return a vector of shape (n_dim,n)
+        '''
+
+        return
+
+
+
+    def distance(self,seeds,pool):
+        '''
+        Calculate the distance between two sets of tracks (seeds) and (pools). Will return a numpy array of distances.
+        Input: seeds is a list of track_uris (strings)
+               pool is a list
+        Output: will return a vector of shape (n_dim,n)
+        '''
+
+        return
+
