d862447cdf86b92d926b0c8047fb93678e8071e3
beangoben <beangoben@gmail.com>
2018-04-30 02:44:12 -0400

    artist, track lookup

--- src/features/tracks_info.py
+++ src/features/tracks_info.py
@@ -8,23 +8,30 @@ from data import compressed_pickle as cpick
 DATA_PATH = '../data/interim'
 PLIST_PATH = '../data/raw'
 
+def data_path(a):
+    return join_path(DATA_PATH,a)
 
 class TrackInfo(object):
 
     def __init__(self, subset=''):
-        #self.data
-        #../data/interim/5k_playlist2track_ints.pkl.bz2
-        #self.datafile = join_path(DATA_PATH, '5k_playlist2track_ints.pkl.bz2')
-        afile = join_path(DATA_PATH, '{}playlist2track_ints.pkl.bz2'.format(subset))
+
+        afile = data_path('{}playlist2track_ints.pkl.bz2'.format(subset))
         self.plists = cpick.load(afile)
-        afile = join_path(DATA_PATH, '{}track_int2track_uri.pkl.bz2'.format(subset))
+        # track information
+        afile = data_path('{}track_int2track_uri.pkl.bz2'.format(subset))
         self.tint2turi = cpick.load(afile)
-        afile = join_path(DATA_PATH, 'track_uri2track_name.pkl.bz2')
+        afile = data_path('track_uri2track_name.pkl.bz2')
         self.turi2tname = cpick.load(afile)
-        afile = join_path(DATA_PATH, 'track_uri2artist_uri.pkl.bz2')
+        afile = data_path('track_uri2artist_uri.pkl.bz2')
         self.turi2auri = cpick.load(afile)
-        afile = join_path(DATA_PATH, 'artist_uri2artist_name.pkl.bz2')
+        # artist information
+        afile = data_path('artist_uri2artist_name.pkl.bz2')
         self.auri2aname = cpick.load(afile)
+        afile = data_path('artist_name2artist_uri.pkl.bz2')
+        self.aname2auri = cpick.load(afile)
+        afile = data_path('artist_uri2track_uri.pkl.bz2')
+        self.auri2turi = cpick.load(afile)
+
     # def track_df(self, ids):
     #     return self.df.ilocs[ids]
 
@@ -40,8 +47,18 @@ class TrackInfo(object):
         return plist
 
     def get_playlist(self, pid):
-        return self.get_playlist_turi(pid),self.get_playlist_tints(pid),self.get_playlist_auri(pid)
+        return self.get_playlist_turi(pid),self.get_playlist_auri(pid)
 
+    def track_info2uris(self,track_name,artist_name):
+        auris = self.aname2auri[artist_name]
+        for auri in auris:
+            tracks = [(t,self.turi2tname[t])  for t in self.auri2turi[auri]]
+            for turi,tname in tracks:
+                if tname == track_name:
+                    return turi,auri
+
+        raise ValueError('Cannot find {} - {}'.format(track_name,artist_name))
+        return
 
     def uri2track_info(self,uri):
         tname = self.turi2tname[uri]
