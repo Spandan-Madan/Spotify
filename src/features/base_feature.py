bd2b6451ee44278f87fa9eea8ea4e5f30514f6b5
beangoben <beangoben@gmail.com>
2018-05-09 01:08:47 -0400

    added distance features

--- src/features/base_feature.py
+++ src/features/base_feature.py
@@ -1,23 +1,11 @@
 from sklearn.metrics.pairwise import pairwise_distances
 import numpy as np
-
-
-def sortlists_by_dist(pool, dist):
-    a, b = (list(t) for t in zip(*sorted(zip(dist, pool), reverse=True)))
-    return b, a
-
-
-def labels2proportions(labels, n):
-    labels_n = len(labels)
-    w_piece = n / float(len(labels))
-    weights = [(key, int(item * w_piece))
-               for key, item in Counter(labels).items()]
-    return [int(w) for i, w in sorted(weights, key=lambda tup: tup[0])]
-
+from sklearn.cluster import k_means
+from medoids import k_medoids
+from math import ceil
 
 def ncluster_ruleofthumb(n):
     return int(ceil(np.sqrt(n / 2)))
-
 
 class Feature(object):
 
@@ -26,7 +14,7 @@ class Feature(object):
         Feature constructor, will load all relevant data, if using the 5k subset, use the string "5k-"
         Input: can be whatever you need (dimention of embedding, folder,etc)
         '''
-
+        self.metric = 'cosine'
         return
 
     def transform(self, turis):
@@ -40,44 +28,35 @@ class Feature(object):
 
     def mean_distance(self, seed_vec, pool_vec, **kwargs):
         mean_vec = np.mean(seed_vec, axis=0).reshape(1, -1)
-        D = pairwise_distances(mean_vec, pool_vec).reshape(-1, 1)
+        D = pairwise_distances(
+            mean_vec, pool_vec, metric=self.metric).reshape(-1, 1)
         return D.ravel()
 
-    def wcentroids_distance(self, seed_vec, pool_vec, n_clusters=None, **kwargs):
-        # cluster size
+    def all_distance(self, seed_vec, pool_vec, **kwargs):
+        D = pairwise_distances(seed_vec, pool_vec, metric=self.metric)
+        return np.min(D, axis=0)
+
+    def centroid_distance(self, seed_vec, pool_vec, n_clusters = None, **kwargs):
         n_clusters = ncluster_ruleofthumb(
-            len(vecs)) if n_clusters is None else n_clusters
-        n_clusters = len(vecs) if n_clusters > len(vecs) else n_clusters
+            len(seed_vec)) if n_clusters is None else n_clusters
+        n_clusters = len(seed_vec) if n_clusters > len(
+            seed_vec) else n_clusters
 
-        centers, labels, inertia = k_means(vecs, n_clusters)
-        props = labels2proportions(labels, n)
+        centroid_vec, labels, inertia = k_means(seed_vec, n_clusters)
 
-        # find most similar
-        pool_dist = []
-        pool_turi = []
-        for center, n_pick in zip(centers, props):
-            D = pairwise_distances(mean_vec, pool_vec).reshape(-1, 1)
+        D = pairwise_distances(centroid_vec, pool_vec, metric=self.metric)
+        return np.min(D, axis=0)
 
-            results = self.model.wv.similar_by_vector(center, topn=n_pick)
-            pool_dist = pool_dist + [i[1] for i in results]
-            pool_turi = pool_turi + \
-                [i[0].replace('spotify:track:', '') for i in results]
-        pool_turi, pool_dist = sortlists_by_dist(pool_turi, pool_dist)
-        return pool_turi, pool_dist
+    def medoid_distance(self, seed_vec, pool_vec, n_clusters = None, **kwargs):
+        n_clusters = ncluster_ruleofthumb(
+            len(seed_vec)) if n_clusters is None else n_clusters
+        n_clusters = len(seed_vec) if n_clusters > len(
+            seed_vec) else n_clusters
 
-    def split_distance(self, seed_vec, pool_vec, **kwargs):
+        medoid_vec, labels = k_medoids(seed_vec, n_clusters=n_clusters)
 
-        # find most similar
-        pool_dist = []
-        pool_turi = []
-        n_pick = ceil(n / len(seed_vec))
-        for vec in vecs:
-            results = self.model.wv.similar_by_vector(vec, topn=n_pick)
-            pool_dist = pool_dist + [i[1] for i in results]
-            pool_turi = pool_turi + \
-                [i[0].replace('spotify:track:', '') for i in results]
-        pool_turi, pool_dist = sortlists_by_dist(pool_turi, pool_dist)
-        return pool_turi, pool_dist
+        D = pairwise_distances(medoid_vec, pool_vec, metric=self.metric)
+        return np.min(D, axis=0)
 
     def distance_between_sets(self, seeds, pool, strat, **kwargs):
         '''
@@ -90,5 +69,12 @@ class Feature(object):
         pool_vec = self.transform(pool)
         if strat == 'mean':
             return self.mean_distance(seed_vec, pool_vec)
-
+        elif strat == 'all':
+            return self.all_distance(seed_vec, pool_vec)
+        elif strat == 'centroid':
+            return self.mean_distance(seed_vec, pool_vec)
+        elif strat == 'medoid':
+            return self.mean_distance(seed_vec, pool_vec)
+        else:
+            raise ValueError('{} nor implemented'.format(strat))
         return
